3f894912649fb3d593f7f947a7f660de6b5c4d42
juyeong <juyeongno1@naver.com>
2022-07-03 16:24:48 +0900

    snapkit,Then

--- SnapKitStudy/SnapKitStudy/ViewController.swift
+++ SnapKitStudy/SnapKitStudy/ViewController.swift
@@ -7,71 +7,26 @@
 
 import UIKit
 import SnapKit
+import Then
+
 
 class ViewController: UIViewController {
-    var redView = UIView()
-    var orangeView = UIView()
-    var blueView = UIView()
-    var greenView = UIView()
-    var indigoView = UIView()
-    var purpleView = UIView()
-    var yellowView = UIView()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(redView)
-        self.view.addSubview(orangeView)
-        self.view.addSubview(yellowView)
-        self.view.addSubview(greenView)
-        self.view.addSubview(blueView)
-        self.view.addSubview(indigoView)
-        self.view.addSubview(purpleView)
-        redView.backgroundColor = .red
-        orangeView.backgroundColor = .orange
-        blueView.backgroundColor = .blue
-        greenView.backgroundColor = .green
-        indigoView.backgroundColor = .systemIndigo
-        purpleView.backgroundColor = .purple
-        yellowView.backgroundColor = .yellow
-        // Do any additional setup after loading the view.
-        redView.snp.makeConstraints { make in
-            make.top.equalTo(view.snp.top)
-            make.size.width.height.equalTo(100)
-            make.left.equalTo(view.snp.left)
+        setUpView()
+    }
+    let label = UILabel().then{
+        $0.text = "center"
+        $0.textColor = .black
+        $0.font = UIFont.systemFont(ofSize: 40, weight: .bold)
+        $0.textAlignment = .center
+        $0.textColor = .red
+    }
+    func setUpView(){
+        self.view.addSubview(label)
+        label.snp.makeConstraints {
+            $0.center.equalTo(view.snp.center)
         }
-        orangeView.snp.makeConstraints { make in
-            make.top.equalTo(redView.snp.bottom)
-            make.size.width.height.equalTo(100)
-            make.left.equalTo(redView.snp.left)
-        }
-        yellowView.snp.makeConstraints { make in
-            make.top.equalTo(orangeView.snp.bottom)
-            make.size.width.height.equalTo(100)
-            make.left.equalTo(orangeView.snp.left)
-        }
-        greenView.snp.makeConstraints { make in
-            make.top.equalTo(yellowView.snp.top)
-            make.size.width.height.equalTo(100)
-            make.left.equalTo(yellowView.snp.right)
-        }
-        blueView.snp.makeConstraints { make in
-            make.top.equalTo(greenView.snp.bottom)
-            make.size.width.height.equalTo(100)
-            make.left.equalTo(greenView.snp.left)
-        }
-        indigoView.snp.makeConstraints { make in
-            make.top.equalTo(blueView.snp.bottom)
-            make.size.width.height.equalTo(100)
-            make.left.equalTo(blueView.snp.left)
-        }
-        purpleView.snp.makeConstraints { make in
-            make.top.equalTo(indigoView.snp.bottom)
-            make.size.width.height.equalTo(100)
-            make.left.equalTo(indigoView.snp.left)
-        }
-
     }
 
-
 }
-
